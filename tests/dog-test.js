a2032fcf0e49dbf867652060052c18581061c7f7
Mathieu Sylvain <mathieu@ti-coco.com>
2012-03-30 08:47:16 -0400

    Simplify the extension syntax with .use() and a common loop handler

--- tests/dog-test.js
+++ tests/dog-test.js
@@ -1,52 +1,54 @@
-var Chain = require("../lib/plastic");
+var Plastic = require("../lib/plastic");
 
 // A Dog constructor
 function Dog(name) {
 	this.name = name;
 	this.distance = 0;
+	this.jumps = [];
 }
 
 // Create a chainable model with the Dog model
-var Dogs = new Chain(Dog);
-
-// Add a single jump method that receives all dogs that are in the context
-// Requires you to handle the forEach
-Dogs.onAll("jump", function(howHigh, dogs) {
-	dogs.forEach(function (dog) {
-		console.log(dog.name + " jumps " + howHigh + " feet high!");
-	});
-});
-
-Dogs.onAll({
-	bark: function(context) {
-		context.forEach(function (dog) {
+var Dogs = new Plastic(Dog, {
+	bark: function(loop, dogs) {
+		return loop(function (dog) {
 			console.log(dog.name + " goes 'BARK!'");
 		});
 	},
-	walk: function(howFar, context) {
-		context.forEach(function(dog) {
+	walk: function(howFar, loop, dogs) {
+		return loop(function(dog) {
 			console.log(dog.name + " walked " + howFar + " meters away!");
 			dog.distance = dog.distance + howFar;
 		});
 	}
 });
 
-// Add multiple methods that receive each dog that are in the context one by one
-// Simpler since there is no need to handle the forEach
-Dogs.onEach("sleep", function(howLong, dog) {
-	console.log(dog.name + " is sleeping for " + howLong + " hours!");
+// Add a single jump method that receives all dogs that are in the context
+// Requires you to handle the forEach
+Dogs.use("jump", function(howHigh, loop, dogs) {
+	return loop(function (dog) {
+		dog.jumps.push(howHigh);
+		console.log(dog.name + " jumps " + howHigh + " feet high!");
+	})
 });
 
 // Add multiple methods
-Dogs.onEach({
-	"stay": function(dog) {
-		console.log(dog.name + " is doing nothing!");
+Dogs.use({
+	sleep: function(howLong, loop, dogs) {
+		return loop(function(dog) {
+			console.log(dog.name + " is sleeping for " + howLong + " hours!");
+		});
 	},
-	"die": function(dog) {
-		console.log(dog.name + " is dead!");
+	"stay": function(loop, dogs) {
+		return loop(function(dog) {
+			console.log(dog.name + " is doing nothing!");
+		});
+	},
+	"die": function(loop, dogs) {
+		return loop(function(dog) {
+			console.log(dog.name + " is dead!");
+		});
 	}
 });
-
 
 var dogs = Dogs(); // Create a new empty collection of dogs
 
@@ -55,19 +57,19 @@ var threeSleepingDogs = dogs
 	.create("Ricky")
 	.create("Sparky")
 	.tap(function (dogs) {
-		console.log("dogs: ", dogs);
+		//console.log("dogs: ", dogs);
 	})
 	.create("Princess")
 	.create("Bobby")
 	.tap(function (dogs) {
-		console.log("dogs: ", dogs);
+		//console.log("dogs: ", dogs);
 	})
 	.all() // Select all available dogs
 	.walk(15)
 	.jump(3)
 	.sleep(6)
 	.tap(function (dogs) {
-			console.log("threeSleepingDogs: ", dogs);
+			//console.log("threeSleepingDogs: ", dogs);
 		});
 
 // Only ricky should die
@@ -75,5 +77,5 @@ var deadRicky = dogs
 		.create("Ricky")
 		.die()
 		.tap(function (dogs) {
-				console.log("deadRicky: ", dogs);
+				//console.log("deadRicky: ", dogs);
 			});
