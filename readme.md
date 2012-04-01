6e1a7d2310c61f77d4b62625ffed36b30fed73d0
Mathieu Sylvain <mathieu@ti-coco.com>
2012-04-01 00:01:32 -0400

    Added tests

--- readme.md
+++ readme.md
@@ -2,6 +2,21 @@
 
 Manipulate your data with a fluent API (Data à la jQuery)
 
-# Introduction
+## Project status
 
-...
+This project is still under development. You should not use it for production purposes yet.
+
+## Introduction
+
+Not yet...
+
+## Tests
+
+Even if the tests are not extensive yet, I try to keep the test coverage at 100%.
+You can use expesso to run the tests. The "lib-cov" folder contains the instrumented
+version of the code in order to obtain the code-coverage report during tests.
+
+### Test commands
+- rm -rf lib-cov
+- node-jscoverage lib lib-cov
+- expresso
---
+++ test/plasticModel-test.js
@@ -0,0 +1,125 @@
+var
+//		plasticModel = require("../lib/plasticModel"),
+		plasticModel = require("../lib-cov/plasticModel"),
+		//vows = require('vows'),
+    	assert = require('assert');
+
+// A Dog constructor
+function Dog(name) {
+	this.name = name;
+	this.distance = 0;
+	this.jumps = [];
+	this.dead = false;
+}
+
+exports.testsStringLength = function(beforeExit, assert) {
+
+	// Create a chainable model with the Dog model
+
+	var Dogs = plasticModel(Dog, {
+		bark: function(dogs) {
+			return this.loop(function (dog) {
+				//console.log(dog.name + " goes 'BARK!'");
+			});
+		},
+		walk: function(dogs, howFar) {
+			return this.loop(function(dog) {
+				//console.log(dog.name + " walked " + howFar + " meters away!");
+				dog.distance = dog.distance + howFar;
+				// It's not needed to return the dog, but it tests wether
+				// you can return any object back into the context in
+				// replacement
+				return dog;
+			});
+		}
+	});
+
+
+	// Add a single jump method that receives all dogs that are in the context
+	// Requires you to handle the forEach
+	Dogs.use("jump", function(dogs, howHigh) {
+		return this.loop(function (dog) {
+			dog.jumps.push(howHigh);
+			//console.log(dog.name + " jumps " + howHigh + " feet high!");
+		})
+	});
+
+	// Add multiple methods
+	Dogs.use({
+		sleep: function(dogs, howLong) {
+			return this.loop(function(dog) {
+				//console.log(dog.name + " is sleeping for " + howLong + " hours!");
+			});
+		},
+		noDeadDogs: function(dogs) {
+			return this.loop(function(dog) {
+				if (dog.dead) return false;
+			});
+		},
+		stay: function(dogs) {
+			return this.loop(function(dog) {
+				//console.log(dog.name + " is doing nothing!");
+			});
+		},
+		die: function(dogs) {
+			return this.loop(function(dog) {
+				dog.dead = true;
+				//console.log(dog.name + " is dead!");
+			});
+		}
+	});
+
+	var dogs = Dogs(); // Create a new empty collection of dogs
+
+	Dogs.on("walk", function (dogs, howFar) {
+		//console.log("Bob is walking along " + dogs.length + " dogs for " + howFar + " meters!");
+	});
+
+	Dogs.on("jump", function (dogs, howFar) {
+		//console.log("Bob is jumping along " + dogs.length + " dogs for " + howFar + " meters!");
+	});
+
+	Dogs.onEach("walk", function (dog, howFar) {
+		//console.log("Bob is walks along " + dog.name + " for " + howFar + " meters!");
+	});
+
+	var threeSleepingDogs = dogs
+		.create("Fido") // Create a dog by calling the original constructor with these parameters
+		.create("Ricky")
+		.create("Sparky")
+		.tap(function (dogs) {
+			//console.log("dogs: ", dogs);
+		})
+		.create("Princess")
+		.create("Bobby")
+		.tap(function (dogs) {
+			//console.log("dogs: ", dogs);
+		})
+		.all() // Select all available dogs
+		.walk(15)
+		.tap(function (dogs) {
+			//console.log("dogs: ", dogs);
+		})
+		.jump(3)
+		.sleep(6)
+		.tap(function (dogs) {
+			//console.log("threeSleepingDogs: ", dogs);
+		});
+
+	// Only ricky should die
+	var dyingRicky = dogs.create("Ricky");
+
+	//todo: add "on" and "onEach" event listening on the chain instance
+	/*
+	dyingRicky.onEach("die", function(dog) {
+		//console.log("Bob's dog " + dog.name + " is dead!!! So sad!");
+	});
+	*/
+	dyingRicky.die();
+
+	var allLivingDogs = dogs.all().noDeadDogs();
+	// Only five dogs in the context
+	console.log(allLivingDogs);
+	assert.equal(allLivingDogs.context.length, 5);
+
+};
